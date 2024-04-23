c62dd29eb804db944c979d0e50b207b01f8990c0
karen.heap8875 <karen.heap8875@stu.mtec.edu>
2024-04-23 14:06:30 -0600

    April 23

--- test/specs/Zipcode.Test.js
+++ test/specs/Zipcode.Test.js
@@ -1,14 +1,15 @@
 import { $ } from '@wdio/globals'
 import { expect } from '@wdio/globals'
+import ZipcodePageFiles from '../zipcode/Zipcode.Page.Files'
 
 
 describe('zipcode test', () => {
     it('testing the zipcode component in all the ways'), async () => {
-        await browser.url('https://www.tesla.com/drive?selectedModel=models')
-        await $('.zipcode-modal-container .tds-link').click()
-        await $('input[placeholder="Zip Code"]').setValue('90210')
-        await $('button.tds-btn').click()
-        await $('.location-card-container .location-card p').getText()
+        await ZipcodePageFiles.ZipcodeURL()
+        await ZipcodePageFiles.ZipCodeBox.click()
+        await ZipcodePageFiles.EnterZipcode.setValue(ZipCodeArray)
+        await ZipcodePageFiles.ConfirmBtn.click()
+        await ZipcodePageFiles.VerifyState.getText(StateArray)
         
         await expect($('.location-card-container .location-card p')).toHaveText('CA')
     
--- test/zipcode/Zipcode.Page.Files.js
+++ test/zipcode/Zipcode.Page.Files.js
@@ -1,9 +1,5 @@
 import { browser } from '@wdio/globals'
 
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
 export default class ZipcodePageFiles {
     /**
     * Opens a sub page of the page
@@ -12,4 +8,36 @@ export default class ZipcodePageFiles {
     open (path) {
         return browser.url(`https://www.tesla.com/drive?selectedModel=models`)
     }
+
+    get ZipCodeURL () {
+        return 'https://www.tesla.com/drive?selectedModel=models'
+    }
+
+    get ZipCodeBox () {
+        return $('.zipcode-modal-container .tds-link')
+    }
+
+    get EnterZipcode () {
+        return $('input[placeholder="Zip Code"]')
+    }
+
+    get ConfirmBtn () {
+        return $('button.tds-btn')
+    }
+
+    get VerifyState () {
+        return $('.location-card-container .location-card p')
+    }
+
+    var ZipCodeArray = [20705, 15701, 36532, 08816, 60452, 19020, 98801, 08060, 60060, 11729, 08648, 15601, 01701, 08844, 30705, 10301, 37205, 10573, 27587, 06514, 21244, 06512, 06010, 60046, 32065, 60148, 44077, 07728, 40165, 60187, 21044, 60048, 59601, 33009, 03301, 75126, 30047, 01906, 21234, 59715, 06095, 42301, 78023, 07087, 67037, 11419, 60073, 44052, 97603, 32404]
+
+    for(let i = 0; i < ZipCodeArray.length; i++)
+
+    var StateArray = [“MD", "PA", "AL", "NJ", "IL", "PA", "WA", "NJ", "IL", "NY", "NJ", "PA", "MA", "NJ", "GA", "NY", "TN", "NY", "NC", "CT", "MD", "CT", "CT", "IL", "FL", "IL", "OH", "NJ", "KY", "IL", "MD", "IL", "MT", "FL", "NH", "TX", "GA", "MA", "MD", "MT", "CT", "KY", "TX", "NJ", "KS", "NY", "IL", "OH", "OR", "FL”];
+    
+    for(let i = 0; i < StateArray.length; i++)
+
 }
+
+
+export default new ZipcodePageFiles();
