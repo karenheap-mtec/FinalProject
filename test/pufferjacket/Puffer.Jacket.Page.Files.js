f140f75b8d0352b5a5b70c698129e11d4be5cebe
Karen Heap <karen.heap8875@mtec.stu.edu>
2024-04-29 10:59:16 -0600

    April 29

--- test/pufferjacket/Puffer.Jacket.Page.Files.js
+++ test/pufferjacket/Puffer.Jacket.Page.Files.js
@@ -2,15 +2,21 @@ import { browser } from '@wdio/globals'
 import { $ } from '@wdio/globals'
 import { expect } from '@wdio/globals'
 
+const SizeXSSelector = '#XS1740535-00-A'
+
 class PufferJacketPage {
     open () {
         return browser.url(`https://shop.tesla.com/product/women_s-puffer-jacket?sku=1740535-00-A`)
     }
 
-    get SizeXS () {
-       // return $('#XS1740535-00-A');
+    // get SizeXSSelector(){
+    //     return '#XS1740535-00-A'
+    // }
+
+    get SizeXSElement () {
+        return $(SizeXSSelector);
         //var htmlContent = $('div input#XS1740535-00-A').getHTML();
-        return $('.product-form.size-1740535-00-A');
+        //return $('.product-form.size-1740535-00-A');
         }
 
     get BtnAddToCart () {
@@ -21,8 +27,21 @@ class PufferJacketPage {
         return $('//div[input[@id="XS1740536-00-A"]]')
     }
 
+    get SizeM () {
+
+    }
+
+    get sizeL () {
+
+    }
+
+    get sizeXL () {
+
+    }
+
     get BtnViewCart () {
-        return $('use[href="#tds-icon-cart"]')
+        return $('a[aria-label="See your cart"]')
+        //return $('use[href="#tds-icon-cart"]')
     }
 
     get InsideCart () {
@@ -30,12 +49,27 @@ class PufferJacketPage {
     }
 
     async addAllItemsToCart () {
-            await this.SizeXS.click()
+            //document.querySelector('#XS1740535-00-A').click()
+            //await this.SizeXS.waitForExist({ timeout: 5000 });
+            browser.execute(() => {
+                document.querySelector(SizeXSSelector).click();
+             });
+             //await this.SizeXS.click()
             await this.BtnAddToCart.click()
-            await this.SizeS.click()
-            await this.BtnAddToCart.click()
-            await this.BtnViewCart.click()
-            await expect(this.InsideCart).toHaveText('Puffer Jacket')
+            // // waitforexist here
+            // await this.SizeS.click()
+            // await this.BtnAddToCart.click()
+            // // waitforexist here
+            // await this.SizeM.click()
+            // await this.BtnAddToCart.click()
+            // // waitforexist here
+            // await this.sizeL.click()
+            // await this.BtnAddToCart.click()
+            // // waitforexist here
+            // await this.sizeXL.click()
+            // await this.BtnAddToCart.click() 
+             await this.BtnViewCart.click()
+             //await expect(this.InsideCart).toHaveText('Puffer Jacket')
     }
 }
 
